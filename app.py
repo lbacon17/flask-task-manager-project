43e3f13731c883321dc39b43aed7b2de3944afad
lbacon17 <louis.bacon17@icloud.com>
2020-12-23 23:33:18 +0000

    Create base template

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import (Flask, flash, render_template, redirect, request, session, url_for)
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -18,10 +20,11 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    return "Hello world... once mooore"
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port = int(os.environ.get("PORT")),
+            port=int(os.environ.get("PORT")),
             debug=True)
